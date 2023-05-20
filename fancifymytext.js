323ff1e4500e693b019eb528c99fbe2e9720b2e1
Elijah Semeryuk <esemeryuk@gmail.com>
2023-05-19 22:23:36 -0700

    completed a5

--- fancifymytext.js
+++ fancifymytext.js
@@ -3,23 +3,40 @@ function helloWorld() {
 }
 
 function bigger() {
-    const text = document.getElementById('inputText-1');
-    text.style.fontSize = "2em";
+    document.getElementById("inputText-1").style.fontSize='2em';
+    //const text = document.getElementById('inputText-1');
+    //text.style.fontSize = '2em';
 }
 
-function fancyify() {
+function fancify() {
     const text = document.getElementById('inputText-1');
-    text.style.fontWeight = 'bold';
-    text.style.color = 'blue';
-    text.style.textDecoration = 'underline';
+    document.getElementById("inputText-1").style.fontWeight = "bold";
+    document.getElementById("inputText-1").style.color = "blue";
+    document.getElementById("inputText-1").style.textDecoration = "underline";
     this.checked = false;
 }
 
 function removeStyle() {
     const text = document.getElementById('inputText-1');
-    text.style.fontSize = "1em";
-    text.style.fontWeight = 'normal';
-    text.style.color = 'black';
+    text.style.fontSize = '1em';
+    text.style.fontWeight = "normal";
+    text.style.color = "black";
     text.style.textDecoration = 'none';
     this.checked = false;
 }
+
+
+function moo()
+{
+    var text = document.getElementById('inputText-1').value;
+    text = text.toUpperCase();
+    var parts = text.split(" ");
+    for (var i = 0; i < parts.length - 1; i++){
+        let word = parts[i].trim();
+        word += "-Moo.";
+        parts[i] = word;
+    }
+    text = parts.join(" ");
+    document.getElementById("inputText-1").value = text;
+}
+
