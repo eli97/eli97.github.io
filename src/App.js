d545d6b22eafd98c3ccb9a007c729d004e4cd0c6
Eli <esemeryuk@gmail.com>
2024-07-25 16:44:32 -0700

    Basic File Structure and Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React from 'react';
+import { Container, Grid } from '@mui/material';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+/* by default, html is a 12x12 grid, as are all elements*/
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Container> {/*imported from material UI, rescales elements in it*/}
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3}> 
+        {/*Define how much space to take based on screen size - ranges from xl to xs, 1 - 12*/}
+          <Profile />
+        </Grid>
+        <Grid item xs>
+        {/*just the size category takes the remaining space */}  
+          <Header />
+          <Router>
+            <Routes>
+            <Route path='/portfolio' element={<Portfolio />}>
+            </Route>
+            <Route path ='/Resume' element={<Resume />}>
+            </Route>
+            </Routes>
+          </Router>
+          
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+/*used rafce code snippet, creates a functional component
+A functional component is a JavaScript component that returns a react element*/
+
+const Profile = () => {
+  return (
+    <div>
+        This is a profile component
+    </div>
+  )
+}
+
+export default Profile
