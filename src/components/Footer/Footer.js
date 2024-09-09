03f56f0aee6997f1a6701780814a9ff2ad915673
Eli <esemeryuk@gmail.com>
2024-09-09 12:47:44 -0700

    progress on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
+import "./Footer.css";
+import resumeData from "../../utils/resumeData";
 
 const Footer = () => {
   return (
-    <div>
-        This is a footer component 
+    <div className="footer">
+      <div className="footer_left">
+        <Typography className="footer_name"></Typography>
+      </div>
+      <div className="footer_right">
+        <Typography className="copy_right">
+          Designed and Developed by{" "}
+          <a
+            href={resumeData.socials.LinkedIn.link}
+            target="_blank"
+            rel="noreferrer">
+            Elijah Semeryuk
+          </a>
+          <br></br>
+          Clone idea from{" "}
+          <a
+            href="https://themeforest.net/user/tavonline"
+            target="_blank"
+            rel="noreferrer">
+            tavonline
+          </a>
+        </Typography>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
